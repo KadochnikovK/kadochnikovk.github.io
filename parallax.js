a2645d56181e707bed0c8fda16e40d3c0e013256
kadochnikov <kadochnikov@papillon.ru>
2023-09-05 12:35:48 +0500

    add files
    Signed-off-by: kadochnikov <kadochnikov@papillon.ru>

---
+++ parallax.js
@@ -0,0 +1,30 @@
+const parallax = document.querySelector('.parallax')
+const parallaxLayers = parallax.querySelectorAll('.parallax__layer')
+
+window.addEventListener('scroll', (e) => {
+    let scrollValue = window.scrollY
+    parallaxLayers.forEach(layer => {
+        layer.style.top = (scrollValue * layer.dataset.parallaxrate * (-1)) + 'px'
+        // layer.style.transform = 'translateY(' + (scrollValue * layer.dataset.parallaxrate * (-1)) + 'px)'
+        // console.log(layer.style.top)
+        // console.log(scrollValue * layer.dataset.parallaxrate * (-1))
+    })
+})
+
+window.addEventListener('mousemove', (e) => {
+
+    parallaxLayers.forEach(layer => {
+
+        if (!layer.dataset.mousemove) {
+            layer.style.transform = `translate(
+            ${((window.innerWidth / 2 - e.clientX) * layer.dataset.parallaxrate / 20)*(-1)}px, 
+            ${((window.innerHeight / 2 - e.clientY) * layer.dataset.parallaxrate / 20)*(-1)}px
+        )`
+        }
+
+        // console.log(layer.style.transform)
+        // console.log(layer.style.top)
+        // console.log(scrollValue * layer.dataset.parallaxrate * (-1))
+    })
+    // console.log(e)
+})
